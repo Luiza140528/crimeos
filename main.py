e7059f2a76caf5684bb592c8978c6bb29f147ce7
Luiza140528 <luizasoares0310@gmail.com>
2026-05-15 18:03:41 -0300

    Update logging messages in main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-
 import logging
+import time
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("CrimeOS")
-
-log.info("Iniciando CrimeOS...")
-
+log.info("Iniciando...")
 from robot import rodar
-import time
-
-log.info("Rodando PT...")
 rodar("PT")
 time.sleep(10)
-log.info("Rodando EN...")
 rodar("EN")
-log.info("Finalizado!")
+log.info("Fim!")
+
 
 
 
