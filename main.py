a8906469c6560b052fce31ce78f7824f083f51b0
Luiza140528 <luizasoares0310@gmail.com>
2026-05-15 17:33:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("CrimeOS")
+
+log.info("Iniciando CrimeOS...")
+
 from robot import rodar
 import time
+
+log.info("Rodando PT...")
 rodar("PT")
 time.sleep(10)
+log.info("Rodando EN...")
 rodar("EN")
-
+log.info("Finalizado!")
 
 
 
